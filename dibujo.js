de6d4e9de7f819d586c6a82a2cb3de9a1ed41213
HiltonRv <x3hiltonx3@gmail.com>
2019-09-17 01:33:52 -0500

    first commit

---
+++ dibujo.js
@@ -0,0 +1,15 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+
+function dibujoPorClick()
+{
+  
+  alert("todo bien");
+}
